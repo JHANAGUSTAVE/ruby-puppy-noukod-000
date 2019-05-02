6405fc0c1d2d1ecc97d94ac820263adacd655650
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-02 17:57:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  attr_accessor :name, :age, :breed, :shelter, :owner
+  @@all = []
+  
+  def initialize(name, age, breed)
+    @name = name
+    @age = age 
+    @breed = breed
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
